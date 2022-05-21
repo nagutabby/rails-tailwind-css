30f42c8fabc05264804c1ff028cd67490cdd86df
nagutabby <nagutabby@gmail.com>
2022-05-22 05:58:15 +0900

    RDSに接続するための環境変数を記述し、DBをMariaDBに変更した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_21_052216) do
-  create_table "todos", force: :cascade do |t|
+  create_table "todos", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
     t.string "priority"
     t.date "limit"
